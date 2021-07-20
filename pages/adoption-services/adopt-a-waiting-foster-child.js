0c9d1942c577373489be4f1fde4e262820982afd
BaskInNature77 <55386039+saiteja-konda@users.noreply.github.com>
2021-07-20 17:50:38 +0530

    file creation with components

---
+++ pages/adoption-services/adopt-a-waiting-foster-child.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Layout from '../../components/core/Layout'
+import { Box, Center, Container, Heading, ListItem, Table, Tbody, Td, Text, Tr, UnorderedList } from "@chakra-ui/react"
+import { Row, Col } from 'react-simple-flex-grid';
+import "react-simple-flex-grid/lib/main.css";
+
+function AdoptionServices() {
+    return (
+        <Layout>
+            <Box mt="8" mb="72">
+                <Container maxW='container.lg'>
+
+                </Container>
+            </Box>
+
+        </Layout>
+    )
+}
+
+export default AdoptionServices
